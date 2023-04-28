a35c6e11b2792098f3e2864c982fc35244aa6138
Danijel Mohači <danijelmo9999@gmail.com>
2023-04-28 18:40:41 +0200

    dodao igru skocko

--- app/src/main/java/com/ftn/ma_sit_project/fragments/SkockoFragment.java
+++ app/src/main/java/com/ftn/ma_sit_project/fragments/SkockoFragment.java
@@ -10,35 +10,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.ftn.ma_sit_project.R;
-import com.google.android.material.navigation.NavigationView;
 
+public class SkockoFragment extends Fragment {
 
-public class WhoKnowsFragment extends Fragment {
-
-    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fragment_who_knows, container, false);
-
-
-        Button btnNext = view.findViewById(R.id.btn_who_knows);
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getParentFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.fragment_container, new SkockoFragment())
-                        .setReorderingAllowed(true)
-                        .commit();
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_skocko, container, false);
     }
 
     @Override
