35dd6d4bde394621b481b8a1a61c4a675e9e8315
marko <crncevicmarko7@gmail.com>
2023-04-29 16:17:46 +0200

    jjos nesto

--- app/src/main/java/com/ftn/ma_sit_project/fragments/StepByStepFragment.java
+++ app/src/main/java/com/ftn/ma_sit_project/fragments/StepByStepFragment.java
@@ -2,20 +2,54 @@ package com.ftn.ma_sit_project.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ftn.ma_sit_project.R;
 
 public class StepByStepFragment extends Fragment {
-
+    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_step_by_step, container, false);
+        view = inflater.inflate(R.layout.fragment_step_by_step, container, false);
+        TextView next = view.findViewById(R.id.step_by_stepTextView_points_right);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new MyNumberFragment())
+                        .setReorderingAllowed(true)
+                        .commit();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+
+        DrawerLayout drawerLayout = getActivity().findViewById(R.id.drawer_layout);
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+
+        DrawerLayout drawerLayout = getActivity().findViewById(R.id.drawer_layout);
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
     }
 }
